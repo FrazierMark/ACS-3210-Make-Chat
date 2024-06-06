3f6a12ad03d0e65ce5516e41fafd6bb6400e6def
FrazierMark <m.mark.frazier@gmail.com>
2024-06-06 12:07:49 -0700

    Integrated Socket.io

--- app.js
+++ app.js
@@ -2,16 +2,23 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const { engine } = require('express-handlebars');
 
-//Express View Engine for Handlebars
-const exphbs  = require('express-handlebars');
-app.engine('handlebars', exphbs());
+//Socket.io
+const io = require('socket.io')(server);
+io.on('connection', (socket) => {
+	console.log('🔌 New user connected! 🔌');
+});
+//Handlebars
+app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 
+app.use('/public', express.static('public'))
+
 app.get('/', (req, res) => {
-  res.render('index.handlebars');
-})
+	res.render('index.handlebars');
+});
 
 server.listen('3000', () => {
-  console.log('Server listening on Port 3000');
-})
+	console.log('Server listening on Port 3000');
+});
